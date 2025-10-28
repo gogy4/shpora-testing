d5b1e8aea096f153817e9fbe16988a5773b4bc06
younggogy <younggogy@gmail.com>
2025-10-28 15:36:09 +0500

    Изменил тест в ObjectComparison.cs с использованием библиотеки FluentAssertion.
    Отрефакторил NumberValidatorTests.cs с использованием FluentAssertion и TestCase

--- Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
+++ Testing/Basic/Homework/1. ObjectComparison/ObjectComparison.cs
@@ -1,31 +1,46 @@
-﻿using NUnit.Framework;
+﻿using System.Text.RegularExpressions;
+using FluentAssertions;
+using FluentAssertions.Equivalency;
+using NUnit.Framework;
 using NUnit.Framework.Legacy;
 
 namespace HomeExercise.Tasks.ObjectComparison;
-public class ObjectComparison
+public partial class ObjectComparison
 {
     [Test]
     [Description("Проверка текущего царя")]
     [Category("ToRefactor")]
-    public void CheckCurrentTsar()
+    public void CheckCurrentTsar_IgnoringIds()
     {
         var actualTsar = TsarRegistry.GetCurrentTsar();
 
         var expectedTsar = new Person("Ivan IV The Terrible", 54, 170, 70,
             new Person("Vasili III of Russia", 28, 170, 60, null));
 
-        // Перепишите код на использование Fluent Assertions.
-        ClassicAssert.AreEqual(actualTsar.Name, expectedTsar.Name);
-        ClassicAssert.AreEqual(actualTsar.Age, expectedTsar.Age);
-        ClassicAssert.AreEqual(actualTsar.Height, expectedTsar.Height);
-        ClassicAssert.AreEqual(actualTsar.Weight, expectedTsar.Weight);
-
-        ClassicAssert.AreEqual(expectedTsar.Parent!.Name, actualTsar.Parent!.Name);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Age, actualTsar.Parent.Age);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Height, actualTsar.Parent.Height);
-        ClassicAssert.AreEqual(expectedTsar.Parent.Parent, actualTsar.Parent.Parent);
+        //можно оставить так, но вдруг появиться свойство, название которого будет начинаться с Id, или иметь Id в середине наименования.
+        /*actualTsar
+            .Should()
+            .BeEquivalentTo(expectedTsar, opt => 
+            opt.Excluding(info => info.Path.EndsWith("Id")));*/
+        
+        actualTsar
+            .Should()
+            .BeEquivalentTo(expectedTsar, opt =>
+            opt.Excluding((IMemberInfo memberInfo) => MyRegex().IsMatch(memberInfo.Name)));
     }
-
+    
+    
+     /*
+     * 1. Если упадет тест мы увидим сообщение из разряда:
+     * Expected: True
+     * But was: False
+     * что не является информативным сообщением, и мы не поймем в чем ошибка конкретно.
+     * 2. Методе AreEqual реализует то, что уже имеют библиотеки, например .BeEquivalentTo, т.е. мы изобретаем велосипед заново.
+     * 3. Возвращаем булевый результат, из за чего теряем контекст ошибки(аналогично п.1)
+     * 4. Метод жестко проверяет свойство parent, и если добавить новое свойство, например Child, тогда тест будет падать, либо нужно будет добавлять новые проверки
+     * 5. Если добавлять впринципе новые свойства/поля тест упадет, или нужно будет добавлять новые проверки
+     * 6. Может уйти в бесконечную рекурсию, если ссылка parent будет идти обратно к исходному обьекту.
+     */
     [Test]
     [Description("Альтернативное решение. Какие у него недостатки?")]
     public void CheckCurrentTsar_WithCustomEquality()
@@ -49,4 +64,7 @@ public class ObjectComparison
             && actual.Weight == expected.Weight
             && AreEqual(actual.Parent, expected.Parent);
     }
+
+    [GeneratedRegex(@"(^Id\b|\bId$)")]
+    private static partial Regex MyRegex();
 }
--- Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
+++ Testing/Basic/Homework/2. NumberValidator/NumberValidatorTests.cs
@@ -1,31 +1,98 @@
-﻿
+﻿using FluentAssertions;
 using NUnit.Framework;
-using NUnit.Framework.Legacy;
+using System.Collections;
 
 namespace HomeExercise.Tasks.NumberValidator;
 
 [TestFixture]
 public class NumberValidatorTests
 {
-    [Test]
-    public void Test()
-    {
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, true));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
-        Assert.Throws<ArgumentException>(() => new NumberValidator(-1, 2, false));
-        Assert.DoesNotThrow(() => new NumberValidator(1, 0, true));
+    #region Конструктор
 
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("00.00"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(17, 2, true).IsValidNumber("0.0"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+0.00"));
-        ClassicAssert.IsTrue(new NumberValidator(4, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("+1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(17, 2, true).IsValidNumber("0.000"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("-1.23"));
-        ClassicAssert.IsFalse(new NumberValidator(3, 2, true).IsValidNumber("a.sd"));
+    [TestCase(-1, 2)]
+    [TestCase(1, -1)]
+    [TestCase(3, 3)]
+    [TestCase(3, 4)]
+    public void Constructor_ShouldThrow_WhenArgsInvalid(int precision, int scale)
+    {
+        var act = () => new NumberValidator(precision, scale);
+        act.Should().Throw<ArgumentException>($"precision={precision}, scale={scale} недопустимы");
     }
+
+    [TestCase(1, 0, true)]
+    [TestCase(10, 5, false)]
+    public void Constructor_ShouldNotThrow_WhenArgsValid(int precision, int scale, bool onlyPositive)
+    {
+        var act = () => new NumberValidator(precision, scale, onlyPositive);
+        act.Should().NotThrow($"precision={precision}, scale={scale}, onlyPositive={onlyPositive} допустимы");
+    }
+
+    #endregion
+
+    #region Валидация чисел
+
+    public static IEnumerable ValidNumbers => new[]
+    {
+        new TestCaseData(17, 2, true, "0.0").SetName("Valid_0.0"),
+        new TestCaseData(4, 2, true, "+1.23").SetName("Valid_+1.23"),
+        new TestCaseData(4, 2, false, "-1.23").SetName("Valid_-1.23"),
+        new TestCaseData(5, 0, true, "12345").SetName("Valid_Integer")
+    };
+
+    [Test, TestCaseSource(nameof(ValidNumbers))]
+    public void IsValidNumber_ShouldReturnTrue_ForValidNumbers(int precision, int scale, bool onlyPositive,
+        string input)
+    {
+        var validator = new NumberValidator(precision, scale, onlyPositive);
+        validator
+            .IsValidNumber(input)
+            .Should()
+            .BeTrue(
+                $"Тест '{TestContext.CurrentContext.Test.Name}' провален: " +
+                $"'{input}' должно быть валидным для precision={precision}, scale={scale}");
+    }
+
+    public static IEnumerable InvalidNumbers => new[]
+    {
+        new TestCaseData(3, 2, true, "+0.00", "число превышает допустимое количество знаков").SetName("Invalid_+0.00"),
+        new TestCaseData(7, 2, true, "-1.231", "число превышает допустимое количество знаков после запятой").SetName(
+            "Invalid_-1.231"),
+        new TestCaseData(3, 2, true, "a.sd", "содержит недопустимые символы").SetName("Invalid_Chars"),
+        new TestCaseData(3, 2, true, "", "пустая строка").SetName("Invalid_Empty"),
+        new TestCaseData(3, 2, true, " ", "строка содержит только пробел").SetName("Invalid_Space"),
+        new TestCaseData(3, 2, true, "+", "строка содержит только знак без числа").SetName("Invalid_SignOnly"),
+        new TestCaseData(3, 2, true, "-", "строка содержит только знак без числа").SetName("Invalid_SignOnlyMinus"),
+        new TestCaseData(2, 0, true, "211", "число превышает допустимую длину целой части").SetName(
+            "Invalid_TooLongInteger"),
+        new TestCaseData(3, 0, true, "0.1", "число превышает допустимую длину дробной части").SetName(
+            "Invalid_ScaleExceeds"),
+        new TestCaseData(17, 2, true, "0.000", "число превышает допустимую длину дробной части").SetName(
+            "Invalid_ScaleExceeds2"),
+        new TestCaseData(3, 0, true, "0.-12", "недопустимый знак внутри числа").SetName("Invalid_InternalSign"),
+        new TestCaseData(3, 0, true, "0.+12", "недопустимый знак внутри числа").SetName("Invalid_InternalSignPlus"),
+        new TestCaseData(3, 0, true, "0.12.12", "число содержит несколько разделителей").SetName(
+            "Invalid_MultipleSeparators"),
+        new TestCaseData(3, 0, true, "0.,12.12", "число содержит несколько разделителей").SetName(
+            "Invalid_MixedSeparators"),
+        new TestCaseData(3, 0, true, "0.2,12", "число содержит несколько разделителей").SetName(
+            "Invalid_MixedSeparators2"),
+        new TestCaseData(3, 0, true, "0,2.12", "число содержит несколько разделителей").SetName(
+            "Invalid_MixedSeparators3"),
+        new TestCaseData(3, 2, true, null, "значение null недопустимо").SetName("Invalid_Null")
+    };
+
+    [Test, TestCaseSource(nameof(InvalidNumbers))]
+    public void IsValidNumber_ShouldReturnFalse_ForInvalidNumbers(int precision, int scale, bool onlyPositive,
+        string input, string expectedMessage)
+    {
+        var validator = new NumberValidator(precision, scale, onlyPositive);
+        validator
+            .IsValidNumber(input)
+            .Should()
+            .BeFalse(
+                $"Тест '{TestContext.CurrentContext.Test.Name}' провален: " +
+                $"'{input ?? "null"}'. {expectedMessage}. " + $"Параметры: precision={precision}, scale={scale}");
+    }
+
+    #endregion
 }
